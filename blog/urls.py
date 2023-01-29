8f07175aa5444382899ff6ecf7266b711ecfd351
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-01-29 21:37:38 +0300

    reverted to previous urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="blog_home"),
     path("about/", views.AboutView.as_view(), name="blog_about"),
-    path("programming/", views.ProgrammingView.as_view(), name="blog_chats"),
+    path("programming/", views.ProgrammingView.as_view(), name="blog_programming"),
     path("tutorials/", views.TutorialView.as_view(), name="blog_tutorials"),
     path("contact/", views.contact, name="blog_contact"),
     path("search/", views.SearchView.as_view(), name="blog_search"),
