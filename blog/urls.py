eadc5ca93058aa6fd9ba09c82c72f0912d659bf7
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-01-29 18:39:22 +0300

    added templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("programming/", views.ProgrammingView.as_view(), name="blog_chats"),
-    path("tutorials/", views.TutorialView.as_view(), name="blog_tutorials"),
     path("", views.home_view, name="blog_home"),
     path("about/", views.AboutView.as_view(), name="blog_about"),
+    path("programming/", views.ProgrammingView.as_view(), name="blog_chats"),
+    path("tutorials/", views.TutorialView.as_view(), name="blog_tutorials"),
     path("contact/", views.contact, name="blog_contact"),
     path("search/", views.SearchView.as_view(), name="blog_search"),
     path("post/<slug:slug>/", views.post_detail_view, name="post_detail"),
