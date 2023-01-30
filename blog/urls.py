e7df791cb4bc8770054829d3879320c86731bb98
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-01-30 16:43:27 +0300

    changed category view from CBV to FBV

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="blog_home"),
     path("about/", views.AboutView.as_view(), name="blog_about"),
-    path("programming/", views.ProgrammingView.as_view(), name="blog_programming"),
-    path("tutorials/", views.TutorialView.as_view(), name="blog_tutorials"),
+    path("programming/", views.programmingView, name="blog_programming"),
+    path("tutorials/", views.tutorialView, name="blog_tutorials"),
     path("contact/", views.contact, name="blog_contact"),
     path("search/", views.SearchView.as_view(), name="blog_search"),
     path("post/<slug:slug>/", views.post_detail_view, name="post_detail"),
